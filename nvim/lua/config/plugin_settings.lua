931218d1ddce3c33641f29873511291e0048d2a6
Yoni Bettan <yonibettan@gmail.com>
2025-07-24 11:09:40 +0300

    Moved all the `fatih/vim-go` configurations to its plugin file.
    After moving to Lua, there is an option to add configuration and
    keybinds that are specific to the plugin to be specified directly in the
    plugin file.
    
    Signed-off-by: Yoni Bettan <yonibettan@gmail.com>

--- nvim/lua/config/plugin_settings.lua
+++ nvim/lua/config/plugin_settings.lua
@@ -27,25 +27,6 @@ function! LightlineFilename()
 endfunction
 ]])
 
--- vim-go
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "go",
-  callback = function()
-    vim.api.nvim_buf_set_keymap(0, "i", ".", ".<C-x><C-o>", { noremap = true })
-  end,
-})
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "go",
-  callback = function()
-    vim.keymap.set("n", "<C-p>", ":GoDef<CR>", { buffer = true, noremap = true, silent = true })
-  end,
-})
-g.go_highlight_types = 1
-g.go_highlight_extra_types = 1
-g.go_highlight_fields = 1
-g.go_fmt_command = "goimports"
-g.go_auto_type_info = 1
-
 -- jedi-vim
 g["jedi#show_call_signatures"] = "2"
 g["jedi#goto_command"] = "<C-p>"
--- nvim/lua/plugins/vim-go.lua
+++ nvim/lua/plugins/vim-go.lua
@@ -3,7 +3,20 @@ return {
   {
     "fatih/vim-go",
     build = ":GoUpdateBinaries",
-    ft = "go"  -- Optional: load the plugin only for go files.
+    ft = "go",  -- Optional: load the plugin only for go files.
+    config = function()
+      -- Set gofmt command to goimports
+      vim.g.go_fmt_command = "goimports"
+      -- Enable auto type info
+      vim.g.go_auto_type_info = 1
+      -- Highlight types, extra types, and fields
+      vim.g.go_highlight_types = 1
+      vim.g.go_highlight_extra_types = 1
+      vim.g.go_highlight_fields = 1
+    end,
+    keys = {
+        {"<C-p>", ":GoDef<CR>", mode = "n", ft = {"go"}, desc = "Jump to definition"},
+        {".", ".<C-x><C-o>", mode = "i", ft = {"go"}, noremap = true, desc = "Insert dot and trigger omni-completion in Go files"},
+    },
   },
 }
-
