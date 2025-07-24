328905733eea18584825c58045592582d6472e2e
Yoni Bettan <yonibettan@gmail.com>
2025-07-24 13:25:20 +0300

    Moving `itchyny/lightline.vim` plugins configurations to the plugin files.
    After moving to Lua based configs and the Lazy plugin-manager, plugins
    can now be configured separately instead of globally.
    
    Signed-off-by: Yoni Bettan <yonibettan@gmail.com>

--- nvim/lua/config/plugin_settings.lua
+++ nvim/lua/config/plugin_settings.lua
@@ -3,29 +3,5 @@ local g = vim.g
 -- supertab
 g.SuperTabDefaultCompletionType = "<c-n>"
 
--- lightline
-g.lightline = {
-  active = {
-    left = {
-      { "mode", "paste" },
-      { "gitbranch", "readonly", "filename", "modified" },
-    },
-  },
-  component_function = {
-    gitbranch = "FugitiveHead",
-    filename = "LightlineFilename",
-  },
-}
-
--- Lightline filename function
-vim.cmd([[
-function! LightlineFilename()
-  return &filetype ==# 'vimfiler' ? vimfiler#get_status_string() :
-       \ &filetype ==# 'unite' ? unite#get_status_string() :
-       \ &filetype ==# 'vimshell' ? vimshell#get_status_string() :
-       \ expand('%') !=# '' ? expand('%') : '[No Name]'
-endfunction
-]])
-
 -- gh-line
 g.gh_open_command = 'fn() { echo "$@" | xclip; }; fn '
--- nvim/lua/plugins/lightline.lua
+++ nvim/lua/plugins/lightline.lua
@@ -2,5 +2,45 @@
 return {
   {
     'itchyny/lightline.vim',
+    config = function()
+      -- Define the LightlineFilename function in Lua
+      vim.fn["lightline#component_expand"] = vim.fn["lightline#component_expand"] or {}
+
+      -- Register the global Lua function for lightline to call
+      vim.cmd([[
+        function! LightlineFilename()
+        return v:lua.LightlineFilename()
+      endfunction
+      ]])
+
+      _G.LightlineFilename = function()
+        local ft = vim.bo.filetype
+        if ft == "vimfiler" then
+          return vim.fn["vimfiler#get_status_string"]()
+        elseif ft == "unite" then
+          return vim.fn["unite#get_status_string"]()
+        elseif ft == "vimshell" then
+          return vim.fn["vimshell#get_status_string"]()
+        elseif vim.fn.expand("%") ~= "" then
+          return vim.fn.expand("%")
+        else
+          return "[No Name]"
+        end
+      end
+
+      -- Set lightline configuration to use that function
+      vim.g.lightline = {
+        active = {
+          left = {
+            { "mode", "paste" },
+            { "gitbranch", "readonly", "filename", "modified" },
+          },
+        },
+        component_function = {
+          gitbranch = "FugitiveHead",
+          filename = "LightlineFilename",
+        },
+      }
+    end,
   },
 }
